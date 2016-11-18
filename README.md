8fe887ab8895453efb5a4d9a18adc97a441a7cfd
rainysia <rainysia@gmail.com>
2016-11-18 19:57:11 +0800

    initial script

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# sync_git_project
+sync_git_project
+==============
 The project can sync all projects you wish to sync with remote repository when you have many projects and wish to use the latest.
+
+
+## Usage:
+---------------------
+```
+    1. Edit sync_git.sh
+    2. Replace or add more array for git_proj_folder, git_folder_branch, git_remote_project
+       variables.
+    3. `git_proj_folder`:  Git project folder name(sometimes equals project name).
+    4. `git_folder_branch`: Git project branch name
+    5. `git_remote_project`: Git project repository alias name, usually called "origin" or "upstream"
+    6. sh ./sync_git.sh path[absolute path or relative path or filename]
+```
+
+## Example
+----------------
+If you have three project named "project1, project2, project3" in /home/www/git/,
+1. Modify sync_git.sh
+2. Change the git_proj_folder, git_folder_branch, git_remote_project like the below:
+```
+git_proj_folders[1]='project1'
+git_proj_folders[2]='project2'
+git_proj_folders[3]='project3'
+```
+3. The git_folder_branch will match the branch you wish to update, if you want to update
+project1->branch:master, project2->branch:develop, project3->branch:test, just change the variables.
+```
+git_folder_branch[1]='master'
+git_folder_branch[2]='develop'
+git_folder_branch[3]='test'
+```
+4. The git_remote_project will match the repository address, if you add others repository coming
+   from co-worker and named as user1, user2, user3, You should change this part, otherwise you 
+   should use the default repository name and use "origin".
+```
+a. Project1 is coming from Tom and wo use `git remote add tom git@github.com:xxx/project1.git` to mark as tom project1.
+b. Project2 is coming from our company project2 and have only one repository, no fork repository.  So the project2 value in here should be "origin"
+c. Project3 is coming from our fork repository and we named as "origin", but we add our company code repository as "upstream" using `git remote add upstream git@github.com:company/project3.git`
+```
+E.g.: 
+```
+git_remote_project[1]='tom'
+git_remote_project[2]='origin'
+git_remote_project[3]='upstream'
+```
+
+#### Contact
+---------------------------------
+Follow me @[rainy_sia](https://twitter.com/rainy_sia) in twitter, [@rainysia](http://weibo.com/rainysia) in weibo, mail me at rainysia#gmail.com 
+
+#### License
+---------------------------------
+Copyright by rainy.sia, 2010 Licensed under the [MIT license](http://www.opensource.org/licenses/mit-license.php)
+
+#### Updates
+---------------------------------
+```
+2016-10-14 14:27:58 Initial the script<br />
+```
---
+++ sync_git.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+# * @author     Rainy Sia <rainysia@gmail.com>
+# * @createTime 2016-10-14 13:35:43
+# * @lastChange 2016-11-18 19:04:55
+#*
+
+set +e
+
+if [ -z "$1" ]; then
+    current_path=`pwd`'/'
+else
+    pre_path=$1
+    if [ "${pre_path:0:1}" = "/" ]; then
+        current_path=$1
+    else
+        current_path=`pwd`/$1
+    fi
+fi
+
+declare git_proj_folders git_folder_branch git_remote_project
+
+git_proj_folders[1]='project1'
+git_proj_folders[2]='project2'
+git_proj_folders[3]='project3'
+git_proj_folders[4]='project4'
+
+git_folder_branch[1]='master'
+git_folder_branch[2]='develop'
+git_folder_branch[3]='stable'
+git_folder_branch[4]='master'
+
+git_remote_project[1]='origin'
+git_remote_project[2]='origin'
+git_remote_project[3]='upstream'
+git_remote_project[4]='upstream'
+
+# Default to git pull with FF merge in quiet mode
+git_command_pull="git pull --quiet"
+git_command_update="git remote update"
+git_command_merge="git merge "
+git_command_stash_save="git stash save"
+git_command_stash_pop="git stash pop"
+git_command_checkout="git checkout "
+git_command_fetch="git fetch "
+git_command_br="git branch | awk '{ print NR=$2 }'"
+git_local_sha="git rev-parse --verify HEAD"
+git_remote_sha="git rev-parse --verify FETCH_HEAD"
+git_command_space=" "
+
+# User messages
+GU_ERR_PROJ="The_git_project_don't_exist."
+GU_ERR_NO_GIT="This_directory_has_not_been_initialized_with_Git."
+GU_ERR_FETCH_FAIL="Unable_to_fetch_the_remote_repository."
+GU_ERR_UPDATE_FAIL="Unable_to_update_the_local_repository."
+GU_ERR_UNKNOWN="Unknown_error"
+GU_ERR_NO_RIGHT="Sorry,_but_we_got_Permission_denied."
+GU_INFO_CURRENT="The_local_repository_is_current._No_update_is_needed."
+GU_INFO_SUCCESS="Update_complete."
+
+pull_all() {
+    for i in "${!git_proj_folders[@]}"
+    do
+        #printf "%s\t%s\n" "$i" "${git_proj_folders[$i]}"
+        proj_name="${git_proj_folders[$i]}"
+        proj_folder="$current_path""${proj_name}"
+        proj_info_no=0
+        proj_info=''
+        proj_style=''
+
+        if [ ! -d "$proj_folder" ]; then
+            proj_info_no=1
+        else
+            cd "$proj_folder"
+            if [[ ! -d ".git" ]]; then
+                proj_info_no=2
+            else
+                cur_branch=`git branch | awk '{ print NR=$2 }'`
+                cur_err=`$git_command_stash_save  2>&1 /dev/null`
+                if [ $? ]; then
+                    if [ $(expr substr "$cur_err" 1 2) == "No" ]; then
+                        ${git_command_fetch}${git_remote_project[$i]}${git_command_space}${git_folder_branch[$i]}
+                        ${git_command_checkout}${git_folder_branch[$i]}
+                        git_run
+                        ${git_command_checkout}${cur_branch}
+                        ${git_command_stash_pop}
+                    elif [ $(expr substr "$cur_err" 1 2) == "fa" ]; then
+                        proj_info_no=4
+                    else
+                        proj_info_no=5
+                    fi
+                else
+                    proj_info_no=10
+                fi
+            fi
+            cd $current_path
+        fi
+
+        if [[ "$proj_info_no" -eq 1 ]]; then
+            proj_info=${GU_ERR_PROJ}
+            proj_style="\033[1;31m[%s]\e[0m: %s\n"
+        elif [[ "$proj_info_no" -eq 2 ]]; then
+            proj_info=${GU_ERR_NO_GIT}
+            proj_style="\033[1;31m[%s]\e[0m: %s\n"
+        elif [[ "$proj_info_no" -eq 3 ]]; then
+            proj_info=${GU_INFO_CURRENT}
+            proj_style="\033[1;32m[%s]\e[0m: %s\n"
+        elif [[ "$proj_info_no" -eq 4 ]]; then
+            proj_info=${GU_ERR_NO_RIGHT}
+            proj_style="\033[1;31m[%s]\e[0m: %s\n"
+        elif [[ "$proj_info_no" -eq 5 ]]; then
+            proj_info=${GU_ERR_UNKNOWN}
+            proj_style="\033[1;31m[%s]\e[0m: %s\n"
+        elif [[ "$proj_info_no" -eq 6 ]]; then
+            proj_info=${GU_ERR_UPDATE_FAIL}
+            proj_style="\033[1;32m[%s]\e[0m: %s\n"
+        else
+            proj_info=${GU_INFO_SUCCESS}
+            proj_style="\033[1;32m[%s]\e[0m: %s\n"
+        fi
+
+        printf "${proj_style}" ${proj_name} ${proj_info}
+    done
+}
+echon() {
+    echo -e "\n"
+}
+
+git_run() {
+    LOCAL_SHA=$(git rev-parse --verify HEAD)
+    REMOTE_SHA=$(git rev-parse --verify FETCH_HEAD)
+    if [ $LOCAL_SHA = $REMOTE_SHA ]; then
+        proj_info_no=3
+    else
+        $git_command_merge${git_remote_project[$i]}/${git_folder_branch[$i]}
+        if (( $? )); then
+            proj_info_no=6
+        else
+            proj_info_no=10
+        fi
+    fi
+}
+
+echon
+pull_all
+echon
+
